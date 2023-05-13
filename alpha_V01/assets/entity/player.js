9d93d6d9dee1c03d416e06683326ed561f3f1cde
AshiyroMisachi <riallot.alexandre@outlook.fr>
2023-05-13 10:57:28 +0200

    Load Sprite

---
+++ alpha_V01/assets/entity/player.js
@@ -0,0 +1,24 @@
+export class Player extends Phaser.Physics.Arcade.Sprite {
+    constructor(scene, x, y) {
+        super(scene, x, y, "perso");
+        scene.add.existing(this);
+        scene.physics.add.existing(this);
+
+        this.init();
+        this.initEvents();
+    }
+
+    init() {
+        
+    }
+
+    initEvents() {
+        
+    }
+
+    update() {
+
+    }
+
+
+}
